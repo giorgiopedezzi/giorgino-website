ba31574a524c71cc09513c2ab0944d555cf9b3fb
Giorgio Pedezzi <giorgio.pedezzi.hqsolutions@gmail.com>
2026-09-16 15:22:12 +0200

    removed a

--- src/components/about/ReallyAboutMe.tsx
+++ src/components/about/ReallyAboutMe.tsx
@@ -58,7 +58,7 @@ export function ReallyAboutMe({arc, content, locale}: {
         value={content.missingMan.heading}/></DisplayHeading>{copy(content.missingMan.body)}<Artifact
         artifact={content.missingMan.artifact} label={content.artifactLabel}/><DisplayHeading as="h3"
                                                                                               className={styles.reflect}><RichTextInline
-        value={content.missingMan.reflection}/>a</DisplayHeading>
+        value={content.missingMan.reflection}/></DisplayHeading>
         <div className={styles.muted}>{copy(content.missingMan.signoff)}</div>
       </div>
     </PageContainer></Section>
